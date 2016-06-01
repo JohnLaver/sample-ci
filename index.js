7f9953892e3dd3d782392f8846a5a3281b91d2ba
John Laver <foo@bar.be>
2016-06-01 17:09:20 +0200

    commmmiiiiit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var api = express();
-
+var DEFAULT_PORT = 3000;
 api.get('/',function(req,res,next){
 	res.send('Hello world');
 });
 
-console.log('server start port 3000');
-api.listen(3000);
+var port = process.env.PORT || DEFAULT_PORT
+console.log('server start port '+port);
+api.listen(port);
 
 module.exports = api;
